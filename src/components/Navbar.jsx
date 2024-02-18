d97b199515e9747ba3b87632641c59d929134677
BenGolan01 <ben@boaideas.com>
2024-02-18 18:58:03 +0200

    save changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,28 +9,28 @@ const Navbar = () => {
 	};
 
 	return (
-		<nav className="bg-white shadow-lg">
+		<nav className="bg-white sticky top-0">
 			<div className="max-w-7xl mx-auto px-4">
-				<div className="flex justify-between items-center py-4">
+				<div className="flex justify-between items-center py-1">
 					<div className="flex items-center">
 						<a href="https://flowbite.com/" className="flex items-center space-x-3">
-							<img src={websiteLogo} className="h-8" alt="Flowbite Logo" />
+							<img src={websiteLogo} className="h-16" alt="Flowbite Logo" />
 						</a>
 					</div>
 					<div className="hidden md:flex space-x-4">
-						<a href="#" className="text-gray-800 hover:text-blue-500">
+						<a href="#" className="text-gray-800 hover:text-teal-500">
 							Home
 						</a>
-						<a href="#" className="text-gray-800 hover:text-blue-500">
+						<a href="#" className="text-gray-800 hover:text-teal-500">
 							About
 						</a>
-						<a href="#" className="text-gray-800 hover:text-blue-500">
+						<a href="#" className="text-gray-800 hover:text-teal-500">
 							Services
 						</a>
-						<a href="#" className="text-gray-800 hover:text-blue-500">
+						<a href="#" className="text-gray-800 hover:text-teal-500">
 							Pricing
 						</a>
-						<a href="#" className="text-gray-800 hover:text-blue-500">
+						<a href="#" className="text-gray-800 hover:text-teal-500">
 							Contact
 						</a>
 					</div>
@@ -38,7 +38,7 @@ const Navbar = () => {
 						<button
 							onClick={toggleMobileMenu}
 							type="button"
-							className="text-gray-800 hover:text-blue-500 focus:outline-none"
+							className="text-gray-800 focus:outline-none"
 						>
 							<svg
 								className={`w-6 h-6 transition-transform transform ${
@@ -61,21 +61,21 @@ const Navbar = () => {
 				</div>
 			</div>
 			{isMobileMenuOpen && (
-				<div className="md:hidden bg-gray-100">
+				<div className="md:hidden bg-gray-100 absolute w-full">
 					<div className="px-4 py-2 space-y-4">
-						<a href="#" className="block text-gray-800 hover:text-blue-500">
+						<a href="#" className="block text-gray-800 hover:text-teal-500">
 							Home
 						</a>
-						<a href="#" className="block text-gray-800 hover:text-blue-500">
+						<a href="#" className="block text-gray-800 hover:text-teal-500">
 							About
 						</a>
-						<a href="#" className="block text-gray-800 hover:text-blue-500">
+						<a href="#" className="block text-gray-800 hover:text-teal-500">
 							Services
 						</a>
-						<a href="#" className="block text-gray-800 hover:text-blue-500">
+						<a href="#" className="block text-gray-800 hover:text-teal-500">
 							Pricing
 						</a>
-						<a href="#" className="block text-gray-800 hover:text-blue-500">
+						<a href="#" className="block text-gray-800 hover:text-teal-500">
 							Contact
 						</a>
 					</div>
